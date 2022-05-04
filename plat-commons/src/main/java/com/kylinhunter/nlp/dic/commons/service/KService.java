56db4d3d7277f2ba8dbaa1cc543560d6c5691f8c
kylinhunter <250917500@qq.com>
2022-05-04 19:08:16 +0800

    init

---
+++ plat-commons/src/main/java/com/kylinhunter/nlp/dic/commons/service/KService.java
@@ -0,0 +1,14 @@
+package com.kylinhunter.nlp.dic.commons.service;
+
+/**
+ * @author BiJi'an
+ * @description a definition for simple service
+ * @date 2022/1/1
+ **/
+public interface KService<T> {
+
+    int getServiceId();
+
+    void setServiceId(int serviceId);
+
+}
