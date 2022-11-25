606838422395c24bdfce3b3ff08f75d480304cf4
kylinhunter <250917500@qq.com>
2022-11-26 01:36:00 +0800

    init

---
+++ plat-core/src/main/java/io/github/kylinhunter/plat/core/Test.java
@@ -0,0 +1,12 @@
+package io.github.kylinhunter.plat.core;
+
+/**
+ * @author BiJi'an
+ * @description
+ * @date 2022-11-26 01:00
+ **/
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
