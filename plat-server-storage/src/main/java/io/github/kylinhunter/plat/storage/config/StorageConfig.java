7d153df622c0d8423d3d68b1a8c9fcbf216190a0
kylinhunter <250917500@qq.com>
2022-11-29 18:52:09 +0800

    init

--- plat-server-storage/src/main/java/io/github/kylinhunter/plat/storage/config/StorageConfig.java
+++ plat-server-storage/src/main/java/io/github/kylinhunter/plat/storage/config/StorageConfig.java
@@ -2,9 +2,11 @@ package io.github.kylinhunter.plat.storage.config;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import io.github.kylinhunter.commons.exception.embed.InitException;
 import io.github.kylinhunter.plat.api.module.storage.constants.StorageType;
 
 import io.github.kylinhunter.commons.util.EnumUtils;
@@ -27,11 +29,15 @@ import lombok.extern.slf4j.Slf4j;
 public class StorageConfig {
     @Getter(AccessLevel.NONE)
     private String type;
+    private String a;
     private S3Config s3;
     private StorageType storageType;
 
     @PostConstruct
     private void init() {
+        if(StringUtils.isEmpty(type)){
+            throw new InitException("init storage config error,type is emtpy");
+        }
         storageType = EnumUtils.fromName(StorageType.class, type.toUpperCase());
         log.info("init storage config ={}", this.toString());
     }
