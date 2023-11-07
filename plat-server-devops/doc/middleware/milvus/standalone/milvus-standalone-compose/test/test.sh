c05a552db7788a8485c2623c3cab4f0b183592b3
kylinhunter <250917500@qq.com>
2023-11-08 02:08:31 +0800

    init

---
+++ plat-server-devops/doc/middleware/milvus/standalone/milvus-standalone-compose/test/test.sh
@@ -0,0 +1,12 @@
+==============================
+测试
+===============================================================================
+
+wget https://raw.githubusercontent.com/milvus-io/pymilvus/v2.3.2/examples/hello_milvus.py
+
+pip install numpy
+
+
+pip install pymilvus
+
+python hello_milvus.py
--- plat-server-devops/doc/middleware/minio/standalone/docker/docker.sh
+++ plat-server-devops/doc/middleware/minio/standalone/docker/docker.sh
@@ -3,6 +3,7 @@
 ===============================================================================
 https://min.io/docs/minio/container/index.html?ref=docs-redirect
 
+https://github.com/minio/minio/blob/RELEASE.2023-09-07T02-05-02Z/docs/docker/README.md?plain=1
 
 ===============================================================================
 安装
@@ -11,10 +12,15 @@ https://min.io/docs/minio/container/index.html?ref=docs-redirect
 docker pull minio/minio:RELEASE.2023-09-07T02-05-02Z
 docker volume create minio-data
 # -d --restart=always \
-docker run -p 9000:9000 -p 9090:9090 \
+
+docker stop minio && docker rm minio
+
+docker run -p 9000:9000 -p 9001:9001 \
+ --network  kylin-net \
+ --network-alias milvus-minio \
  --name minio \
  -e "MINIO_ROOT_USER=minioadmin" \
  -e "MINIO_ROOT_PASSWORD=minioadmin" \
  -v minio-data:/data \
  minio/minio:RELEASE.2023-09-07T02-05-02Z server \
- /data --console-address ":9090"
+ /data --console-address ":9001"
