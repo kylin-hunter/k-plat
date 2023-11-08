3de1780cc9d38b9d15e6fa715cc3daf57ee6b38f
kylinhunter <250917500@qq.com>
2023-11-09 00:09:00 +0800

    init

--- plat-server-search/src/main/resources/db/migration-search/V1.0.1__search.sql
+++ plat-server-search/src/main/resources/db/migration-search/V1.0.1__search.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `kplat_search_syn_progress`
     `sys_update_user_name`  varchar(64) NOT NULL default '' COMMENT '最后编辑人username',
     `sys_update_time`       datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后编辑时间',
     `sys_delete_flag`       tinyint     NOT NULL DEFAULT 0 COMMENT '0 未删除 1删除',
-    `sys_op_lock`           int         NOT NULL DEFAULT 0 COMMENT '乐观锁',
+    `sys_op_lock`           int          NULL DEFAULT 0 COMMENT '乐观锁',
     `next_sync_time`        datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '上次同步时间',
     `last_sync_id`          varchar(64) NOT NULL DEFAULT '' COMMENT '上次同步的ID',
     PRIMARY KEY (`id`)
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `kplat_search_syn_task`
     `sys_update_user_name`  varchar(64) NOT NULL default '' COMMENT '最后编辑人username',
     `sys_update_time`       datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后编辑时间',
     `sys_delete_flag`       tinyint     NOT NULL DEFAULT 0 COMMENT '0 未删除 1删除',
-    `sys_op_lock`           int         NOT NULL DEFAULT 0 COMMENT '乐观锁',
+    `sys_op_lock`           int          NULL DEFAULT 0 COMMENT '乐观锁',
     `type`                  int(11)     NOT NULL DEFAULT 0 COMMENT '3、文档search同步、4、Faq search同步',
     `status`                tinyint(4)  NOT NULL DEFAULT 0 COMMENT '0待同步，1正同步，2成功，3阶段失败，4最终失败',
     `msg`                   varchar(255)         DEFAULT NULL COMMENT '同步信息',
